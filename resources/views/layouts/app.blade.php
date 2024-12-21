0a015637e1302bac041858787846013a3a9bbdf3
ThriF3 <firmanfawnia.f12@gmail.com>
2024-12-21 08:49:12 +0700

    feature: Merubah navbar menjadi sidebar pada tampilan admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,21 +16,26 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+            <div class="flex">
+                @include('components.admin-sidebar')
+                <div class="w-full p-4 sm:ml-64">
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+                    <!-- Page Heading -->
+                    @if (isset($header))
+                        <header class="bg-white shadow">
+                            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                                {{ $header }}
+                            </div>
+                        </header>
+                    @endif
+        
+                    <!-- Page Content -->
+                    <main>
+                        {{ $slot }}
+                    </main>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                </div>
+            </div>
         </div>
     </body>
 </html>
